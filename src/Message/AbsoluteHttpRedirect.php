d050645cd7c60577be56e09f26e0052fdaa2e692
appkit-framework <admin@appkit.biz>
2025-12-29 14:59:50 +0100

    Add AbsoluteHttpRedirect

---
+++ src/Message/AbsoluteHttpRedirect.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppKit\Http\Message;
+
+class AbsoluteHttpRedirect extends HttpResponseException {
+    function __construct(
+        $location,
+        $status = 302,
+        $headers = [],
+        $previous = null
+    ) {
+        parent::__construct(
+            $status,
+            headers: [ 'Location' => $location ] + $headers,
+            previous: $previous
+        );
+    }
+
+    public function getLocation() {
+        return $this -> getHeaderLine('Location');
+    }
+
+    public function withLocation($location) {
+        return $this -> withHeader('Location', $location);
+    }
+}
--- src/Message/HttpRedirect.php
+++ src/Message/HttpRedirect.php
@@ -2,25 +2,4 @@
 
 namespace AppKit\Http\Message;
 
-class HttpRedirect extends HttpResponseException {
-    function __construct(
-        $location,
-        $status = 302,
-        $headers = [],
-        $previous = null
-    ) {
-        parent::__construct(
-            $status,
-            headers: [ 'Location' => $location ] + $headers,
-            previous: $previous
-        );
-    }
-
-    public function getLocation() {
-        return $this -> getHeaderLine('Location');
-    }
-
-    public function withLocation($location) {
-        return $this -> withHeader('Location', $location);
-    }
-}
+class HttpRedirect extends AbsoluteHttpRedirect {}
