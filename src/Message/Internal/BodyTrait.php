6e5f5b7e2096c313dbd04b94c722d94f5816c511
appkit-framework <admin@appkit.biz>
2026-03-26 14:56:14 +0100

    Add body param methods

--- src/Message/Internal/BodyTrait.php
+++ src/Message/Internal/BodyTrait.php
@@ -10,12 +10,25 @@ trait BodyTrait {
         return $this -> body;
     }
 
+    public function hasBodyParam($name) {
+        return isset($this -> body[$name]);
+    }
+
+    public function getBodyParam($name) {
+        return $this -> body[$name] ?? null;
+    }
+
     public function getBodyText() {
         return $this -> bodyText;
     }
 
     protected function setBody($body) {
         $this -> body = $body;
+        return $this;
+    }
+
+    protected function setBodyParam($name, $value) {
+        $this -> body[$name] = $value;
         return $this;
     }
 
